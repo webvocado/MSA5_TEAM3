5d1f2eaee92d2ea13ff52e7f5a884a9786409fbf
downying <tkwk36@naver.com>
2024-03-27 19:33:53 +0900

    20240327

---
+++ javafx_board/src/main/java/com/joeun/Controller/InsertController.java
@@ -0,0 +1,60 @@
+package com.joeun.Controller;
+
+import java.io.IOException;
+
+import com.joeun.App;
+import com.joeun.DTO.Board;
+import com.joeun.Service.BoardService;
+import com.joeun.Service.BoardServiceImpl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class InsertController {
+
+    @FXML
+    private TextArea tContent;
+
+    @FXML
+    private TextField tTtile;
+
+    @FXML
+    private TextField tWriter;
+
+    private BoardService boardService = new BoardServiceImpl();
+
+    // @FXML
+    // void moveToList(ActionEvent event) throws IOException {
+    //     App.setRoot("board/List");
+    // }
+
+    // 글 등록 처리
+    public void insert(ActionEvent event) throws Exception {
+        // insert.fxml에 입력된 데이터를 가져와서 새로운 board 객체 생성
+        Board board = new Board(tTtile.getText(), tWriter.getText(), tContent.getText());
+
+        // insert.fxml에 입력된 데이터 더블체크 (지워도 됨)
+        System.out.println("title: " +  tTtile.getText());
+        System.out.println("writer: " +  tWriter.getText());
+        System.out.println("content: " +  tContent.getText());
+
+        // 새롭게 생성된 board 객체를 boardList에 추가
+        int result = boardService.insert(board);
+
+        // insert() 메서드
+        // 결과 : 0 --> 데이터 등록 실패
+		//       1 --> 데이터 등록 성공
+        if ( result > 0 ) {
+            System.out.println("글쓰기 처리 성공!");
+            // 게시글 목록 화면(메인)으로 이동
+            App.setRoot("board/List");
+        }
+    }
+    // 목록 보기 처리
+    @FXML
+    void moveToList(ActionEvent event) throws IOException {
+        App.setRoot("board/List");
+    }
+}
---
+++ javafx_board/src/main/java/com/joeun/Controller/SelectController.java
@@ -0,0 +1,81 @@
+package com.joeun.Controller;
+
+import java.io.IOException;
+
+import com.joeun.App;
+import com.joeun.DTO.Board;
+import com.joeun.Service.BoardService;
+import com.joeun.Service.BoardServiceImpl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class SelectController {
+
+    @FXML
+    private TextArea tContent;
+
+    @FXML
+    private TextField tSelect;
+
+    @FXML
+    private TextField tTitle;
+
+    @FXML
+    private TextField tWriter;
+
+    static private BoardService boardService =  new BoardServiceImpl();
+
+    @FXML
+    void delete(ActionEvent event) {
+        //글삭제
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+        
+        String boardContent = tContent.getText();
+        String boardTitle = tTitle.getText();
+        String boardWriter = tWriter.getText();
+        Board board = new Board(boardTitle, boardWriter, boardContent);
+        board.setNo( Integer.parseInt(tSelect.getText()) );
+
+        boardService.update(board);
+        
+    }
+
+    @FXML
+    void moveToList(ActionEvent event) throws IOException {
+        // 목록보기
+        App.setRoot("board/list");
+    }
+
+    @FXML
+    void select(ActionEvent event) {
+        Board board = new Board();
+        // //조회버튼 눌렀을 때 작동할 함수
+        board = boardService.select( Integer.parseInt(tSelect.getText()) );
+
+        // 원하는 게시글 잘 찾았는지 체크하는 출력문 (지워도됌)
+        System.out.println(board.getTitle());
+        System.out.println(board.getWriter());
+        System.out.println(board.getContent());
+
+        tTitle.setText(board.getTitle());
+        tWriter.setText(board.getWriter());
+        tContent.setText(board.getContent());
+
+
+    }
+
+    // 선택된 게시글 정보를 받아서 화면에 표시하는 메서드
+    public void setBoard(Board board) {
+        tSelect.setText(String.valueOf(board.getNo())); // 게시글 번호를 텍스트 필드에 설정
+        tTitle.setText(board.getTitle()); // 제목 설정
+        tWriter.setText(board.getWriter()); // 작성자 설정
+        tContent.setText(board.getContent()); // 내용 설정
+    }
+
+}
